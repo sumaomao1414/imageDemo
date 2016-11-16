d73f1c96dd9f7adc17e2c00d916cfee4ec00cdc4
sumaomao1414 <maomao@MacBook-Pro123.local>
2016-11-16 14:19:48 +0800

    YYPhotoGroupView 的简单使用

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '7.0'
+
+target 'imageDemo' do
+  # Uncomment this line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for imageDemo
+
+pod 'SDWebImage', '~> 3.8.1'
+  target 'imageDemoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'imageDemoUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- imageDemo/ViewController.m
+++ imageDemo/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "YYPhotoGroupView.h"
+#import "UIImageView+WebCache.h"
+
 
 @interface ViewController ()
+@property(nonatomic,strong)NSMutableArray *pics;
+@property(nonatomic,strong)NSMutableArray *subViews;
 
 @end
 
@@ -17,7 +22,86 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    CGFloat x = 10;
+    CGFloat y = 5;
+    CGFloat w = self.view.frame.size.width/3 - 10;
+    CGFloat h = 90;
+    
+    _pics = [NSMutableArray array];
+    _subViews = [NSMutableArray array];
+      
+    for (NSInteger i = 0; i < 9; i++) {
+        
+        [_pics addObject:@"https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1466066885&di=7e3dc96da227dc591efa1ec82e24396e&src=http://www.one101.com/htm/fengjxsh/tp/fh/wu-long-shan/da/62.jpg"];
+        UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(x, y, w, h)];
+        imageView.tag = i;
+        imageView.userInteractionEnabled = YES;
+        imageView.layer.masksToBounds = YES;
+        imageView.layer.cornerRadius = 5;
+        imageView.backgroundColor = [UIColor redColor];
+        [imageView sd_setImageWithURL:[NSURL URLWithString:_pics[i]]];
+        imageView.contentMode = UIViewContentModeScaleAspectFit;
+        [self.view addSubview:imageView];
+        [_subViews addObject:imageView];
+        
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(dealTap:)];
+        [imageView addGestureRecognizer:tap];
+        
+        //改变坐标
+        if (i%3 != 2) {
+            x = x + w+5;
+        }
+        else //换行
+        {
+            x=10;
+            y = y +h+5;
+        }
+        
+    }
+
+    
+    
+    
 }
+
+- (void)dealTap:(UITapGestureRecognizer *)tap
+{
+    NSLog(@"%ld",tap.view.tag);
+    UIView *fromView = nil;
+    NSMutableArray *items = [NSMutableArray new];
+
+    for (NSUInteger i = 0, max = _pics.count; i < max; i++) {
+        YYPhotoGroupItem *item = [YYPhotoGroupItem new];
+        item.thumbView = self.subViews[i];
+        item.largeImageURL = [NSURL URLWithString:_pics[i]];
+        item.largeImageSize = tap.view.frame.size;
+        [items addObject:item];
+        if (i == tap.view.tag) {
+            fromView = tap.view;
+        }
+    }
+    
+    UIViewController *mainVc = [UIApplication sharedApplication].keyWindow.rootViewController;
+    
+    YYPhotoGroupView *photoVc = [[YYPhotoGroupView alloc] initWithGroupItems:items];
+    [photoVc presentFromImageView:fromView toContainer:mainVc.view animated:YES completion:nil];
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
